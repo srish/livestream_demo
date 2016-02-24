f032c07cdc5c697a10e50aef50bbfec4634e1473
srishakatux <srishakatux@gmail.com>
2016-02-23 22:23:39 -0500

    now accepts channel name and clipID, changes player content accordingly

--- js/livestream.js
+++ js/livestream.js
@@ -6,9 +6,17 @@ swfobject.embedSWF("http://cdn.livestream.com/chromelessPlayer/v20/playerapi.swf
 
 function switchChannel() {
 	player = document.getElementById("livestreamPlayer");
+	
+	channelName = document.getElementById("channel-name"); 
+	clipId = document.getElementById("clip-id");
+	
 	player.setDevKey(options.devKey);
-	player.load(options.channelName);
+	player.load(channelName.value, clipId.value);
 	player.startPlayback();
+	//clip id is pla_d87c0258-95f9-423f-85f2-d953c7370a76
 }
+
+
+
 
 
